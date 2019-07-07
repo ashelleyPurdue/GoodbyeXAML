8b2d47a79b67fdfcd1d37ef004cf799f1d3b675b
Alex Shelley <alexthepeppershelley@gmail.com>
2019-07-07 17:41:54 -0400

    It now generates one giant .cs file.

--- CodeGenerator/Program.cs
+++ CodeGenerator/Program.cs
@@ -1,23 +1,54 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace CodeGenerator
 {
     class Program
     {
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+
+        [DllImport("Kernel32")]
+        public static extern void FreeConsole();
+
         static void Main(string[] args)
         {
+            AllocConsole();
             GenerateWPFExtensions("foo");
+            Console.ReadKey();
+            FreeConsole();
         }
 
         private static void GenerateWPFExtensions(string outputFolder)
         {
-            Assembly wpfAssembly = Assembly.Load("System.Windows");
-            var types = wpfAssembly.GetTypes();
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
 
-            foreach (Type t in types)
-                Console.WriteLine(t.Name);
+            string outFilePath = Path.Combine(outputFolder, "WPFExtensions.cs");
+            using (var outFile = new StreamWriter(outFilePath))
+            {
+                outFile.WriteLine("namespace GoodbyeXAML.Wpf {");
+
+                // Write the extension method class for every FrameworkElement type.
+                Type frameworkElement = typeof(FrameworkElement);
+
+                var controlTypes = frameworkElement
+                    .Assembly
+                    .GetTypes()
+                    .Where(t => t == frameworkElement || t.IsSubclassOf(frameworkElement))
+                    .OrderBy(t => t.FullName);
+
+                foreach (Type t in controlTypes)
+                {
+                    outFile.WriteLine(ExtensionMethodGenerator.GenerateExtensionClassFor(t));
+                }
+
+                outFile.WriteLine("}");
+            }
         }
     }
 }
