452acbd791678d1d7005b3a4ab9c96702cc1c80e
Alex Shelley <alexthepeppershelley@gmail.com>
2019-07-12 19:36:12 -0400

    Add AllReferencedNamespaces extension method

--- CodeGenerator/TypeExtensions.cs
+++ CodeGenerator/TypeExtensions.cs
@@ -40,5 +40,29 @@ namespace CodeGenerator
 
             return builder.ToString();
         }
+
+        /// <summary>
+        /// Returns all namespaces you would need to import to represent this type
+        /// without fully-qualifying it.
+        /// 
+        /// EG: EventHandler<DragStartedEventArgs> would yield:
+        ///     * System
+        ///     * System.Windows.Controls.Primitives
+        /// because EventHandler belongs to System and DragStartedEventArgs belongs
+        /// to System.Windows.Controls.Primitives
+        /// </summary>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> AllReferencedNamespaces(this Type T)
+        {
+            yield return T.Namespace;
+
+            Type[] genericTypeArgs = T.GetGenericArguments();
+            foreach (Type genericArg in genericTypeArgs)
+            {
+                foreach (string ns in genericArg.AllReferencedNamespaces())
+                    yield return ns;
+            }
+        }
     }
 }
