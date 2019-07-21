ba967bdf5113167c0234768c17256b6806088b0d
Alex Shelley <alexthepeppershelley@gmail.com>
2019-07-20 20:34:50 -0400

    Add LambdaBinding shared project.

---
+++ LambdaBinding/ExpressionUtils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GoodbyeXAML.LambdaBinding
+{
+    internal static class ExpressionUtils
+    {
+        /// <summary>
+        /// Allows easy enumeration of an expresison tree.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IEnumerable<Expression> TraverseExpressionTree(Expression root)
+        {
+            var visitor = new Visitor();
+            visitor.Visit(root);
+            return visitor.nodes;
+        }
+
+        public static IEnumerable<(object owner, string memberName)> GetAllPropertyAccesses(Expression expression)
+        {
+            return TraverseExpressionTree(expression)
+                .Where(e => e is MemberExpression)
+                .Select(e => (MemberExpression)e)
+                .Where(me => me.Expression is MemberExpression)
+                .Select(me => (GetMemberExpressionObject(me), me.Member.Name));
+        }
+
+        public static object GetMemberExpressionObject(MemberExpression me)
+        {
+            // Borrowed from https://stackoverflow.com/questions/1613239/getting-the-object-out-of-a-memberexpression
+            // Thanks @Denis Shishkanov!
+            return Expression.Lambda((MemberExpression)me.Expression)
+                .Compile()
+                .DynamicInvoke();
+        }
+
+        private class Visitor : ExpressionVisitor
+        {
+            public List<Expression> nodes = new List<Expression>();
+            public override Expression Visit(Expression node)
+            {
+                nodes.Add(node);
+                return base.Visit(node);
+            }
+        }
+    }
+}
---
+++ LambdaBinding/WeakKeyDictionary.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoodbyeXAML.LambdaBinding
+{
+    internal class WeakKeyDictionary<TKey, TValue>
+    {
+        private Dictionary<WeakReference<TKey>, TValue> innerDict = new Dictionary<WeakReference<TKey>, TValue>();
+
+        public TValue this[TKey key]
+        {
+            get => innerDict[Weak(key)];
+            set => innerDict[Weak(key)] = value;
+        }
+
+        public ICollection<TKey> Keys => innerDict
+            .Keys
+            .Where(k => k.IsAlive)
+            .Select(k => k.Target)
+            .ToArray();
+
+        public void Add(TKey key, TValue value)
+        {
+            CleanDeadKeys();
+            innerDict.Add(Weak(key), value);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+            => innerDict.TryGetValue(Weak(key), out value);
+
+        /// <summary>
+        /// Deletes all dead keys
+        /// </summary>
+        private void CleanDeadKeys()
+        {
+            // I really wanted to use LINQ here, but this routine will be
+            // called every time we access the dictionary, so it actually
+            // needs to be efficient :(
+
+            var deadKeys = new List<WeakReference<TKey>>();
+            foreach (var key in innerDict.Keys)
+            {
+                if (!key.IsAlive)
+                    deadKeys.Add(key);
+            }
+
+            foreach (var key in deadKeys)
+                innerDict.Remove(key);
+        }
+
+        private WeakReference<TKey> Weak(TKey key) => new WeakReference<TKey>(key);
+    }
+
+    internal class WeakReference<T> : WeakReference
+    {
+        private int originalHashCode;
+
+        public WeakReference(T target) : base(target)
+            => originalHashCode = target.GetHashCode();
+
+        public new T Target
+        {
+            get => (T)base.Target;
+            set => base.Target = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = (WeakReference<T>)obj;
+
+            if (!IsAlive || !other.IsAlive)
+                return false;
+
+            return Target.Equals(other.Target);
+        }
+
+        public override int GetHashCode() => originalHashCode;
+    }
+}
