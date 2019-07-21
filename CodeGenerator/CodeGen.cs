01885bec09b8667368bef01ee646255ff4f3bae1
Alex Shelley <alexthepeppershelley@gmail.com>
2019-07-21 15:01:57 -0400

    Extract csproj generation to a separate class.

--- CodeGenerator/CodeGen.cs
+++ CodeGenerator/CodeGen.cs
@@ -13,25 +13,10 @@ public static class CodeGen
         string projFolder = Path.Combine(outputFolder, projectName);
         string csprojName = Path.Combine(projFolder, projectName + ".csproj");
         string nuspecName = Path.Combine(projFolder, projectName + ".nuspec");
-        string namespaceName = projectName;
 
-        GenerateClassFiles(projFolder, namespaceName, types);
-        File.WriteAllText(csprojName, GenerateCSProj());
+        GenerateClassFiles(projFolder, projectName, types);
+        File.WriteAllText(csprojName, CsProj.GenerateNetCore(projectName));
         File.WriteAllText(nuspecName, Nuspec.Generate(projectName));
-
-        string GenerateCSProj() =>
-        $@"
-            <Project Sdk=""Microsoft.NET.Sdk.WindowsDesktop"">
-                <PropertyGroup>
-                    <OutputType>Library</OutputType>
-                    <TargetFramework>netcoreapp3.0</TargetFramework>
-                    <RootNamespace>{namespaceName}</RootNamespace>
-                    <UseWPF>true</UseWPF>
-                 </PropertyGroup>
-
-                <Import Project=""../../LambdaBinding/LambdaBinding.projitems"" Label=""Shared"" />
-            </Project>
-        ";
     }
 
     public static void GenerateWPFDotnetFrameworkProject(string outputFolder, string projectName, IEnumerable<Type> types)
@@ -40,79 +25,10 @@ public static class CodeGen
         string projFolder = Path.Combine(outputFolder, projectName);
         string csprojName = Path.Combine(projFolder, projectName + ".csproj");
         string nuspecName = Path.Combine(projFolder, projectName + ".nuspec");
-        string namespaceName = projectName;
-        string assemblyName = projectName;
-        const string frameworkVersion = "v4.8";
 
-        GenerateClassFiles(projFolder, namespaceName, types);
-        File.WriteAllText(csprojName, GenerateCSProj());
+        GenerateClassFiles(projFolder, projectName, types);
+        File.WriteAllText(csprojName, CsProj.GenerateNetFramework(projectName, types));
         File.WriteAllText(nuspecName, Nuspec.Generate(projectName));
-
-        string GenerateCSProj() =>
-        $@"
-            <Project ToolsVersion=""15.0"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
-                <Import Project=""$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props"" Condition=""Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')"" />
-                <PropertyGroup>
-                    <Configuration Condition="" '$(Configuration)' == '' "">Debug</Configuration>
-                    <Platform Condition="" '$(Platform)' == '' "">AnyCPU</Platform>
-                    <OutputType>library</OutputType>
-                    <RootNamespace>{namespaceName}</RootNamespace>
-                    <AssemblyName>{assemblyName}</AssemblyName>
-                    <TargetFrameworkVersion>{frameworkVersion}</TargetFrameworkVersion>
-                    <FileAlignment>512</FileAlignment>
-                    <ProjectTypeGuids>{{60dc8134-eba5-43b8-bcc9-bb4bc16c2548}};{{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}}</ProjectTypeGuids>
-                    <WarningLevel>4</WarningLevel>
-                    <Deterministic>true</Deterministic>
-                </PropertyGroup>
-                <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' "">
-                    <DebugSymbols>true</DebugSymbols>
-                    <DebugType>full</DebugType>
-                    <Optimize>false</Optimize>
-                    <OutputPath>bin\Debug\</OutputPath>
-                    <DefineConstants>DEBUG;TRACE</DefineConstants>
-                    <ErrorReport>prompt</ErrorReport>
-                    <WarningLevel>4</WarningLevel>
-                </PropertyGroup>
-                <PropertyGroup Condition="" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' "">
-                    <DebugType>pdbonly</DebugType>
-                    <Optimize>true</Optimize>
-                    <OutputPath>bin\Release\</OutputPath>
-                    <DefineConstants>TRACE</DefineConstants>
-                    <ErrorReport>prompt</ErrorReport>
-                    <WarningLevel>4</WarningLevel>
-                </PropertyGroup>
-                <ItemGroup>
-                    <Reference Include=""System"" />
-                    <Reference Include=""System.Data"" />
-                    <Reference Include=""System.Xml"" />
-                    <Reference Include=""Microsoft.CSharp"" />
-                    <Reference Include=""System.Core"" />
-                    <Reference Include=""System.Xml.Linq"" />
-                    <Reference Include=""System.Data.DataSetExtensions"" />
-                    <Reference Include=""System.Net.Http"" />
-                    <Reference Include=""System.Xaml"">
-                        <RequiredTargetFramework>4.0</RequiredTargetFramework>
-                    </Reference>
-                    <Reference Include=""WindowsBase"" />
-                    <Reference Include=""PresentationCore"" />
-                    <Reference Include=""PresentationFramework"" />
-                </ItemGroup>
-                <ItemGroup>
-                    {GenerateCompileIncludes()}
-                </ItemGroup>
-                <Import Project=""../../LambdaBinding/LambdaBinding.projitems"" Label=""Shared"" />
-                <Import Project=""$(MSBuildToolsPath)\Microsoft.CSharp.targets"" />
-            </Project>
-        ";
-
-        string GenerateCompileIncludes()
-        {
-            var builder = new StringBuilder();
-            foreach (Type t in types)
-                builder.AppendLine($@"<Compile Include=""{t.Name}Extensions.cs"" />");
-
-            return builder.ToString();
-        }
     }
 
     public static void GenerateClassFiles(string outputFolder, string namespaceName, IEnumerable<Type> types)
