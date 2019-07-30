32ed7b55f7f20b011dc4320b18c13800ca3a0758
Alex Shelley <awshelley@beckman.com>
2019-07-30 16:16:05 -0400

    Move logic from GenerateWPFExtensions into GenerateShareProject and make it generic.

--- CodeGenerator/CodeGenerator/CodeGen.cs
+++ CodeGenerator/CodeGenerator/CodeGen.cs
@@ -8,17 +8,37 @@ using CodeGenerator;
 
 public static class CodeGen
 {
-    public static void GenerateSharedProject(string outputFolder, string projectName, string sharedGuid, IEnumerable<Type> types)
+    /// <summary>
+    /// Generates a shared project containing extension methods for all
+    /// the given types, and their subtypes
+    /// </summary>
+    /// <param name="outputFolder">Where the generated files should be placed.</param>
+    /// <param name="projectName"></param>
+    /// <param name="sharedGuid">A GUID for the generated .shproj file.  Visual Studio INSISTS on .shproj's having GUIDs.</param>
+    /// <param name="types">All types that you want extension methods generated for.  Derived types will automatically be roped in, too.</param>
+    public static void GenerateSharedProject(string outputFolder, string projectName, string sharedGuid, params Type[] types)
     {
         string projFolder = Path.Combine(outputFolder, projectName);
         string shprojName = Path.Combine(projFolder, projectName + ".shproj");
         string projitemsName = Path.Combine(projFolder, projectName + ".projitems");
-        
-        GenerateClassFiles(projFolder, projectName, types);
+
+        var derivedTypes = types
+            .SelectMany(t => GetDerivedTypes(t))
+            .Where(t => t.IsPublic)
+            .Where(t => !t.IsGenericType)
+            .Distinct()
+            .OrderBy(t => t.FullName);
+
+        GenerateClassFiles(projFolder, projectName, derivedTypes);
         File.WriteAllText(shprojName, GenerateShProj(projectName, sharedGuid));
         File.WriteAllText(projitemsName, GenerateProjItems(projectName, types, sharedGuid));
     }
-    
+
+    public static IEnumerable<Type> GetDerivedTypes(Type type) => type
+        .Assembly
+        .GetTypes()
+        .Where(t => t == type || t.IsSubclassOf(type));
+
     public static void GenerateClassFiles(string outputFolder, string namespaceName, IEnumerable<Type> types)
     {
         if (!Directory.Exists(outputFolder))
--- CodeGenerator/CodeGenerator/Program.cs
+++ CodeGenerator/CodeGenerator/Program.cs
@@ -20,25 +20,15 @@ namespace CodeGenerator
         static void Main(string[] args)
         {
             AllocConsole();
-            GenerateWPFExtensions(OUTPUT_FOLDER);
+
+            CodeGen.GenerateSharedProject
+            (
+                OUTPUT_FOLDER,
+                "GoodbyeXAML.Wpf.Shared",
+                "38dc387f-0306-4f71-bf34-eb3060308dba",
+                typeof(FrameworkElement)
+            );
             FreeConsole();
-        }
-
-        private static void GenerateWPFExtensions(string outputFolder)
-        {
-            // Write the extension method class for every FrameworkElement type.
-            Type frameworkElement = typeof(FrameworkElement);
-
-            var types = frameworkElement
-                .Assembly
-                .GetTypes()
-                .Where(t => t == frameworkElement || t.IsSubclassOf(frameworkElement))
-                .Where(t => t.IsPublic)
-                .Where(t => !t.IsGenericType)
-                .OrderBy(t => t.FullName);
-
-            const string SHARED_GUID = "38dc387f-0306-4f71-bf34-eb3060308dba";
-            CodeGen.GenerateSharedProject(outputFolder, "GoodbyeXAML.Wpf.Shared", SHARED_GUID, types);
         }
     }
 }
