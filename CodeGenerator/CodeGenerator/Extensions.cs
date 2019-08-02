f7630de41f4e85d457a216b18cdd4c0558275b2e
Alex Shelley <awshelley@beckman.com>
2019-08-02 13:57:34 -0400

    remove unused code.

--- CodeGenerator/CodeGenerator/Extensions.cs
+++ CodeGenerator/CodeGenerator/Extensions.cs
@@ -44,44 +44,5 @@ namespace CodeGenerator
 
             return builder.ToString();
         }
-
-        /// <summary>
-        /// Returns all namespaces you would need to import to represent this type
-        /// without fully-qualifying it.
-        /// 
-        /// EG: EventHandler<DragStartedEventArgs> would yield:
-        ///     * System
-        ///     * System.Windows.Controls.Primitives
-        /// because EventHandler belongs to System and DragStartedEventArgs belongs
-        /// to System.Windows.Controls.Primitives
-        /// </summary>
-        /// <param name="T"></param>
-        /// <returns></returns>
-        public static IEnumerable<string> AllReferencedNamespaces(this Type T)
-        {
-            yield return T.Namespace;
-
-            Type[] genericTypeArgs = T.GetGenericArguments();
-            foreach (Type genericArg in genericTypeArgs)
-            {
-                foreach (string ns in genericArg.AllReferencedNamespaces())
-                    yield return ns;
-            }
-        }
-
-        /// <summary>
-        /// Why does this *not* exist by default?
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="set"></param>
-        /// <param name="contents"></param>
-        public static void AddRange<T>(this HashSet<T> set, IEnumerable<T> contents)
-        {
-            foreach (T item in contents)
-                set.Add(item);
-        }
-
-        public static void AddRange<T>(this HashSet<T> set, params T[] contents) 
-            => set.AddRange((IEnumerable<T>)contents);
     }
 }
